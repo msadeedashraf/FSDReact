e3f8b5ec82a6396153d92841b591ebf7cc69399e
Sadeed <msadeedashraf@gmail.com>
2024-04-30 12:53:43 -0400

    Day Complete

---
+++ Day1/node-example/app.js
@@ -0,0 +1,19 @@
+/*
+//browsers
+edge-----javascript engine is ----chakra
+firFox---javascript engine is ----spiderMonkey
+chrome---javascript engine is ----V8
+
+//node is built using chrome's V8--wrapped it in C++ code and created node.exe
+//lot of libraries openspurce 
+//Modules
+*/
+
+console.log("Hello World from node");
+function addNumber(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addNumber(10, 5));
+
+//console.log(window);
