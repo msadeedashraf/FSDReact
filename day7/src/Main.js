98e35efc52fcde2b52a46dc44cfd898fbad09c01
Sadeed <msadeedashraf@gmail.com>
2024-05-08 11:38:23 -0400

    useState repeat

--- day7/src/Main.js
+++ day7/src/Main.js
@@ -104,6 +104,25 @@ const Main = ({ learn }) => {
   const [color, setColor] = useState("Red");
   console.log(color);
 */
+
+  /*
+  const myCourse = useState("React");
+  //console.log(myCourse);
+  //Array destructuring
+  const [course, setCourse] = useState("JavaScript");
+  console.log(course);
+
+  return (
+    <main>
+      <h1> Hello to learning {course}</h1>
+      <h2> My First course was {course}</h2>
+      <button onClick={() => setCourse("TypeScript")}> Change Course </button>
+      <button onClick={() => setCourse("React")}> Change Course again </button>
+    </main>
+  );
+  */
+
+  /*
   const [course, setCourse] = useState();
   console.log(course);
 
@@ -111,6 +130,37 @@ const Main = ({ learn }) => {
     <main>
       <h1> Hello to learning {course} </h1>
       <button onClick={() => setCourse("JavaScript")}> Change Course </button>
+    </main>
+  );
+  */
+
+  const [course, setCourse] = useState("React");
+  const [count, setCount] = useState(0);
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red",
+  });
+
+  const myClick = () => {
+    setCount();
+
+    console.log(count);
+  };
+
+  console.log(course);
+  return (
+    <main>
+      <h1> Hello to learning {course} </h1>
+
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+
+      <button onClick={() => setCourse("JavaScript")}> Change Course </button>
+      <button onClick={myClick}> click </button>
     </main>
   );
 };
