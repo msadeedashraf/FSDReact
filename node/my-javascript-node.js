f31e561551031bd5c6eac7ac5cbd944c3875e505
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-06-03 13:12:52 -0400

    node day1

---
+++ node/my-javascript-node.js
@@ -0,0 +1,25 @@
+//execute the first js file using node
+// on terminal 
+// >node file_name.js
+
+//console.log("Hello world from node");
+
+//global object
+//console.log(global);
+
+//common core module
+//commonJS module
+
+const os = require('os');
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
+
+
+
+
+
