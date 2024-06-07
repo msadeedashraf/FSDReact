43f96cf274089089698364a6b013bbd17ff21080
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-06-07 13:39:58 -0400

    npm

--- node/stream.js
+++ node/stream.js
@@ -39,6 +39,8 @@ if (!fs.existsSync('./sadeed'))
             
             
             });
+
+            
         
 
     }
---
+++ npm/index.js
@@ -0,0 +1,9 @@
+console.log("hello World from nodemon");
+
+console.log("welcome to CBC");
+
+const {format} = require('date-fns')
+
+
+
+console.log(format(new Date(),'MM/dd/yyyy \t HH:mm:ss'));
