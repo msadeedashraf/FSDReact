54d7242af6d72df15f5514906081825741323167
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-06-11 13:22:30 -0400

    web-server serveFile function start

--- web-server/server.js
+++ web-server/server.js
@@ -12,11 +12,130 @@ const fsPromises = require('fs').promises;
 
 const myPORT = process.env.PORT || 3600;
 
+/*
+const serveFile = async (filePath, contentType, response)=> {
+
+    try{
+
+        
+
+
+    }
+    catch (err) {
+        console.log(err);
+
+
+    }
+
+
+
+}
+*/
+
 //http server
 const myServer =  http.createServer(
 (req, res)=>{
-
 console.log(req.url, req.method);
+
+let filePath;
+/*
+if (req.url === '/'|| req.url === 'index.html')
+    {
+res.statusCode=200;
+res.setHeader('Content-Type', 'text/html');
+filePath = path.join(__dirname, 'views', 'index.html');
+fs.readFile(filePath, 'utf8', (err, data)=>{res.end(data)});
+    }
+*/
+/*
+switch(req.url)
+{
+    case '/':
+        res.statusCode=200;
+        res.setHeader('Content-Type', 'text/html');
+        filePath = path.join(__dirname, 'views', 'index.html');
+        fs.readFile(filePath, 'utf8', (err, data)=>{res.end(data)});
+
+    break;
+    
+}
+*/
+
+const myExtention = path.extname(req.url);
+let contentType;
+
+switch (myExtention)
+{
+
+case '.css': 
+contentType =  'text/css'
+break;
+
+case '.js': 
+contentType =  'text/javascript'
+break;
+
+case '.html': 
+contentType =  'text/html'
+break;
+
+case '.jpg': 
+contentType =  'text/jpeg'
+break;
+case '.png':
+    contentType = 'image/png';
+    break;
+case '.txt':
+    contentType = 'text/plain';
+    break;
+default:
+    contentType = 'text/html';
+
+}
+
+filePath = 
+contentType === 'text/html' && req.url ==='/'
+? path.join(__dirname, 'views', 'index.html')
+: contentType === 'text/html' && req.url.slice(-1) ==='/'
+? path.join(__dirname, 'views', 'index.html')
+: contentType === 'text/html'
+?path.join(__dirname, 'views', req.url)
+:path.join(__dirname, req.url);
+
+if (!myExtention&&req.url.slice(-1) !== '/' )
+    filePath+='.html'  
+
+//console.log(filePath);
+const fileExixst = fs.existsSync(filePath);
+
+if (fileExixst)
+    {
+//get me the file
+//function to serve the page
+//serveFile();
+    }
+else
+{
+    switch(path.parse(filePath).bsse)
+    {
+        case 'old-page.html':
+            res.writeHead(301, { 'Location': '/new-page.html' }); //header is Location and page to server is /new-page.html
+            res.end();
+            break;
+        case 'www-page.html':
+            res.writeHead(301, { 'Location': '/' });
+            res.end();
+            break;
+        default:
+
+
+    }
+
+    console.log(path.parse(filePath));
+    //404
+    //301 redirect
+
+}
 }
 
 
