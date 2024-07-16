8101b24fdd488a12ee3d4d337c9fd3bfd25fbc8f
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-07-16 12:35:59 -0400

    .

--- angular/first-app/README.md
+++ angular/first-app/README.md
@@ -1,5 +1,4 @@
-# HelloWorld
-
+# FirstApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.7.
 
--- angular/hello-world/src/app/component/home/home.component.ts
+++ angular/hello-world/src/app/component/home/home.component.ts
@@ -1,23 +1,20 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HousinglocationComponent} from '../housinglocation/housinglocation.component'
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousinglocationComponent],
+  imports: [CommonModule, HousinglocationComponent],
   template: `
-  <section>
-    <form action="">
-
-    <input type="text" placeholder="Filter by city">
-    <button type="button">Search</button>
-    </form>
-  </section>
-  <section class="result">
-<app-housinglocation>
-
-</app-housinglocation>
-
-  </section>
+   <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housinglocation></app-housinglocation>
+    </section>
   `,
   styleUrl: './home.component.css'
 })
